239a23d87acfe4c3ce6ef46bbd2d5ad0cd545410
mymaking <152869680+mymaking@users.noreply.github.com>
2023-12-24 12:07:49 +0700

    update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,3 @@
 #!/usr/bin bash
 
 gunicorn --bind 0.0.0.0:8080 server.flask:app &
-
-sleep 10 
-
-while true; do
-  gmt7_time=$(TZ=Asia/Ho_Chi_Minh date +"%Y-%m-%d %H:%M:%S")
-  
-  echo "Content Download - Time: $gmt7_time"
-  
-  sleep 169200
-done
