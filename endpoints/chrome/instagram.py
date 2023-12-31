4f219abff8eafa797fd793ce951674872eaa31b7
mymaking <152869680+mymaking@users.noreply.github.com>
2023-12-31 09:22:29 +0000

    update endpoints/chrome/instagram.py

--- endpoints/chrome/instagram.py
+++ endpoints/chrome/instagram.py
@@ -2,14 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import logging
-
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-options.add_argument("--start-maximized")
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-gpu')
-options.add_argument('--disable-dev-shm-usage')
+from endpoints.webset import options
 
 def get(url):
     driver = webdriver.Chrome(options=options)
