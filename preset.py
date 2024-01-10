7e508c9f8eb7054f429abd508a8c733bea4194b6
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-10 21:24:05 +0700

    update preset.py

--- preset.py
+++ preset.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
-options.add_argument("--start-maximized")
+# options.add_argument("--start-maximized")
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-gpu')
-options.add_argument('--disable-dev-shm-usage')
+# options.add_argument('--disable-dev-shm-usage')
