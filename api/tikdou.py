7363c770c4aa9a4ad27cd513422305521189bc7f
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-10 11:58:17 +0700

    update api/tikdou.py

--- api/tikdou.py
+++ api/tikdou.py
@@ -3,8 +3,31 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from preset import options
+from douyin_tiktok_scraper.scraper import Scraper
+import asyncio
+
+api = Scraper()
 
 def get(url):
+    try:
+        file_url, music_url, is_video = asyncio.run(get_api(url))
+    except:
+        file_url, is_video = get_web(url)
+        music_url = None 
+    return file_url, music_url, is_video
+
+async def get_api(url: str) -> dict:
+    result = await api.hybrid_parsing(url)
+    if result.get("video_data"):
+        is_video = True
+        file_url = result["video_data"]["nwm_video_url"]
+    else:
+        is_video = False
+        file_url = result["image_data"]["no_watermark_image_list"]
+    music_url = result["music"]["play_url"]["url_list"][0]
+    return file_url, music_url, is_video
+
+def get_web(url):
   driver = webdriver.Chrome(options=options)
   driver.get('https://snaptik.app/')
   input_element = driver.find_element(By.CLASS_NAME, "link-input")
@@ -15,12 +38,12 @@ def get(url):
   try:
     download_video = wait.until(
     EC.presence_of_element_located((By.CLASS_NAME, "download-file")))
-    fileurl = download_video.get_attribute("href")
-    video = True
+    file_url = download_video.get_attribute("href")
+   is_video = True
   except:
     download_photos = wait.until(
     EC.presence_of_all_elements_located((By.CLASS_NAME, "w100")))
-    fileurl = fileurl = [element.get_attribute("href") for element in download_photos]
-    video = False
+    file_url = file_url = [element.get_attribute("href") for element in download_photos]
+   is_video = False
   driver.quit()
-  return fileurl, video
+  return file_url, is_video
